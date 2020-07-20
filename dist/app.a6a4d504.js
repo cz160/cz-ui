efd4d7f49bc3058fa23f1d523b335f4661b1350e
cz160 <1604506998@qq.com>
2020-07-20 20:13:00 +0800

    feat():修改配置

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -12745,7 +12745,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52598" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52051" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
