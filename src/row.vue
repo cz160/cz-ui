00f3fefecf97246d06e9ec76afe9a46d4e3cbc86
陈昭 <chenz@weipaitang.com>
2020-10-31 13:07:56 +0800

    feat:row组件支持justify属性

--- src/row.vue
+++ src/row.vue
@@ -1,6 +1,7 @@
 <template>
   <div
     class="row"
+    :class="rowClass"
     :style="rowStyle"
   >
     <slot></slot>
@@ -9,15 +10,35 @@
 <style lang="scss" scoped>
 .row {
   display: flex;
+  &.justify-start{
+    justify-content: flex-start;
+  }
+   &.justify-end{
+    justify-content: flex-end;
+  }
+   &.justify-center{
+    justify-content: center;
+  }
 }
 </style>
 <script>
 export default {
   name: "cz-row",
+  data(){
+    return {
+      justifyValue: ['start','end','center']
+    }
+  },
   props: {
     gutter: {
       type: [Number, String],
     },
+    justify:{
+      type:String,
+      validator(value){
+        return ['start','end','center'].includes(value)
+      }
+    }
   },
   mounted() {
     this.$children.forEach((vm) => {
@@ -30,6 +51,10 @@ export default {
         marginLeft: -this.gutter / 2 + "px",
         marginRigth: -this.gutter / 2 + "px",
       };
+    },
+    rowClass() {
+      const { justify } = this;
+      return [ justify && `justify-${justify}` ]
     },
   },
 };
