5112665e25f9affd2a09d9b904d7da7419f9a930
陈昭 <chenz@weipaitang.com>
2020-11-03 16:30:52 +0800

    feat:文档基本结构搭建

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    title: 'cz-ui',
+    description: '一个好用的UI框架',
+    themeConfig: {
+        sidebar: [
+            {
+                title:'入门',
+                children:[
+                    '/install/',
+                    '/get-start/',
+                ]
+            },
+            {
+                title:'组件',
+                children:[
+                    '/components/button'
+                ]
+            }
+        ]
+    }
+}
